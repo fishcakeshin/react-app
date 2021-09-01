7568ea722b8aab455e840bc10330581210619af8
HS <fishcakecake@naver.com>
2021-09-01 16:31:49 +0900

    [state] state를 이용해 여러개의 엘리먼트 생성하기,key값 적용하기 (key값은 엘리먼트에 안정적인 고유성을 부여하기 위해 배열 내부 엘리먼트에 지정)

--- src/App.js
+++ src/App.js
@@ -9,7 +9,12 @@ class App extends Component {
   constructor(props){
     super(props); 
     this.state = {
-      subject:{title:'WEB', sub:'World Wid Web!'}
+      subject:{title:'WEB', sub:'World Wid Web!'},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+      ]
     }
   }
 
@@ -19,7 +24,7 @@ class App extends Component {
         <Subject 
         title={this.state.subject.title} 
         sub={this.state.subject.sub}></Subject>
-        <TOC></TOC>
+        <TOC data={this.state.contents}></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language"></Content>
       </div>
     );
