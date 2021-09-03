ae833eff091a41f49bd91c7e93f3e564da49d2de
HS <fishcakecake@naver.com>
2021-09-03 17:03:49 +0900

    [Create] concat 활용해 contents 변경 추가, 불필요한 alert 삭제

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ import './App.css';
 
 class App extends Component {
   constructor(props){
-    super(props); 
+    super(props);
+    this.max_content_id = 3;
     this.state = {
       mode:'create',
       selected_content_id: 2,
@@ -45,7 +46,14 @@ class App extends Component {
       }else if(this.state.mode === 'create'){
         _article = <CreateContent onSubmit={function(_title, _desc){
           //add content to this.state.contents
+          this.max_content_id = this.max_content_id+1;
           console.log(_title, _desc);
+          var _contents = this.state.contents.concat(
+            {id:this.max_content_id, title:_title, desc:_desc}
+          )
+          this.setState({
+            contents:_contents
+          });
         }.bind(this)}></CreateContent>
     }
     return (
