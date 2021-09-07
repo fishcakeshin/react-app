72d06a0f8591ce3b30aa9a435a081164a30e899f
HS <fishcakecake@naver.com>
2021-09-07 17:47:07 +0900

    [update] form 구현

--- src/components/UpdateContent.js
+++ src/components/UpdateContent.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 
 class UpdateContent extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            title:this.props.data.title,
+            desc:this.props.data.desc
+        }
+        this.inputFormHandler = this.inputFormHandler.bind(this);
+    }
+    inputFormHandler(e){
+        this.setState({[e.target.name]:e.target.value});
+    }
     render(){
         console.log('UpdateContent render');
         return (
@@ -15,8 +26,23 @@ class UpdateContent extends Component{
                     );
                 }.bind(this)}
             >
-            <p><input type="text" name="title" placeholder="title"></input></p>
-            <p><textarea name="desc" placeholder="description"></textarea></p>
+            <p>
+                <input 
+                    type="text" 
+                    name="title"
+                    placeholder="title"
+                    value={this.state.title}
+                    onChange={this.inputFormHandler}
+                ></input>
+            </p>
+            <p>
+                <textarea 
+                    onChange={this.inputFormHandler}
+                    name="desc" 
+                    placeholder="description"
+                    value={this.state.desc}
+                ></textarea>
+            </p>
             <p><input type="submit"></input></p>
             </form>
         </article>
